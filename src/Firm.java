8318add88913d54b537d17cd64393c2c72a36cb5
Liza Bordo <lizaabordo@lewisu.edu>
2019-06-18 11:25:10 -0400

    add

--- src/Firm.java
+++ src/Firm.java
@@ -1,10 +1,35 @@
+import java.util.Scanner;
 
 public class Firm {
 
 	public static void main(String[] args) {
+		float regularPayRate;
+		float specialPayRate;
+		float overtimePayRate;
+		float hoursSpecial;
+		float hoursOvertime;
+		float hoursReg = 0;
+		float hours;
+		
 		
 		Person employee = new Person();
 		
+		
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("How many hours did you work this week?");
+		hours = scan.nextFloat();
+
+		System.out.println("How many special session hours did you work this week?");
+		hoursSpecial = scan.nextFloat();
+
+		if (hours - hoursSpecial <= 30) {
+			hours = hoursReg;
+		}
+
+		if (hours - hoursSpecial > 30) {
+			hoursOvertime = hours - hoursSpecial - 30;
+			hoursReg = hours - hoursOvertime - hoursSpecial;}
 	}
 
 }
--- src/PaidAdvisor.java
+++ src/PaidAdvisor.java
@@ -1,38 +1,19 @@
 
-import java.util.Scanner;
-
 public class PaidAdvisor extends Person {
 
+	public float regularPayRate;
+	public float specialPayRate;
+	public float overtimePayRate;
+	public float hoursSpecial;
+	public float hoursOvertime;
+	public float hoursReg = 0;
+	public float hours;
+	
 	public PaidAdvisor() {
 
-		float regularPayRate;
-		float specialPayRate;
-		float overtimePayRate;
-		float hoursSpecial;
-		float hoursOvertime;
-		float hoursReg = 0;
-		float hours;
-
-		Scanner scan = new Scanner(System.in);
-
-		System.out.println("How many hours did you work this week?");
-		hours = scan.nextFloat();
-
-		System.out.println("How many special session hours did you work this week?");
-		hoursSpecial = scan.nextFloat();
-
-		if (hours - hoursSpecial <= 30) {
-			hours = hoursReg;
+	
 		}
 
-		if (hours - hoursSpecial > 30) {
-			hoursOvertime = hours - hoursSpecial - 30;
-			hoursReg = hours - hoursOvertime - hoursSpecial;
-		}
+	
 
-	}
-
-	private void pay() {
-
-	}
 }
