1535cb3d018c4033e21abe8620112b3e29cc9fb3
Liza Bordo <lizaabordo@lewisu.edu>
2019-06-19 10:46:13 -0400

    add

--- src/Firm.java
+++ src/Firm.java
@@ -3,29 +3,29 @@ import java.util.Scanner;
 public class Firm {
 
 	public static void main(String[] args) {
-		
-		String firstName;
-		String lastName;
+
+	//	long firstName;
+	//	long lastName;
 		float hoursSpecial;
 		float hoursOvertime;
 		float hoursReg = 0;
 		float hours;
 
-		Person employee = new Person();
+		new Person();
 
 		Scanner scan = new Scanner(System.in);
-		System.out.println("What is your first name");
-		firstName = scan.toString();
-		
-		hours = scan.nextFloat();System.out.println("What is your last name?");
-		lastName = scan.toString();
-		
+	//	System.out.println("What is your first name");
+	//	firstName = scan.nextLong();
+
+	//	System.out.println("What is your last name?");
+	//	lastName = scan.nextLong();
+
 		System.out.println("How many hours did you work this week?");
 		hours = scan.nextFloat();
 
 		System.out.println("How many special session hours did you work this week?");
 		hoursSpecial = scan.nextFloat();
-		
+
 		if (hours - hoursSpecial <= 30) {
 			hours = hoursReg;
 		}
@@ -34,18 +34,7 @@ public class Firm {
 			hoursOvertime = hours - hoursSpecial - 30;
 			hoursReg = hours - hoursOvertime - hoursSpecial;
 		}
-		
-	
-			
-		
-			
-			
-			
-		}
-	
 
-
-		
 	}
 
-
+}
