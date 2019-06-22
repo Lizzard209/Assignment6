b139cd9ac84a9d1e6b8833489bf28907f4a0fa27
Liza Bordo <lizaabordo@lewisu.edu>
2019-06-21 22:12:54 -0500

    add

--- src/Firm.java
+++ src/Firm.java
@@ -8,18 +8,20 @@ public class Firm {
 		float hoursOvertime;
 		float hoursReg = 0;
 		float hours;
+		float payRate=0;
+		float hoursWorked=0;
 
 		Scanner scan = new Scanner(System.in);
+
+		Person employee = new Person();
 		
-		 Person employee = new Person();
-		
+
 		System.out.println("What is your first name?");
 		String firstName = scan.next();
 
-
 		System.out.println("What is your last name?");
-		String lastName  = scan.next();
-		
+		String lastName = scan.next();
+
 		System.out.println("How many hours did you work this week?");
 		hours = scan.nextFloat();
 
@@ -34,8 +36,11 @@ public class Firm {
 			hoursOvertime = hours - hoursSpecial - 30;
 			hoursReg = hours - hoursOvertime - hoursSpecial;
 		}
+		employee.setNameRatehours(firstName, lastName, payRate, hoursWorked);
+		//whyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyyy
+	}
 
 	
- //Special hours add 
-}
+	
+
 }
--- src/PaidAdvisor.java
+++ src/PaidAdvisor.java
@@ -1,7 +1,6 @@
 
 public class PaidAdvisor extends Person {
 
-
 	float regularPayRate;
 	float specialPayRate;
 	float overtimePayRate;
@@ -9,63 +8,61 @@ public class PaidAdvisor extends Person {
 	float hoursOvertime;
 	float hoursSpecial;
 	float hoursWorked;
-	float payRate;	
+	float payRate;
 	float totalhoursWorked;
 	String name;
 	float money;
-	
-	
-	
+
 	public PaidAdvisor() {
-	
-		super(); 
-		
-		
+
+		super();
+
 		regularPayRate = 0;
 		specialPayRate = 0;
 		overtimePayRate = 0;
 		hoursReg = 0;
 		hoursOvertime = 0;
 		hoursSpecial = 0;
-		hoursWorked =0;
+		hoursWorked = 0;
 		name = "";
-		money= 0;
+		money = 0;
 	}
 
-	public String toString(String firstName, String lastName, String calculatePay) {
+	public String toString(String firstName, String lastName) {
 		String result = super.toString();
+		float calculatePay = calculatePay();
 		result += calculatePay;
 		return result;
 	}
 
-	public float calculatePay(float payRate, float hoursWorked) {
+	public float calculatePay() {
+		float hoursWorked = hoursReg + hoursOvertime+ hoursSpecial;
+		float hoursOvertime = hoursWorked - hoursSpecial-30;
+		float hoursReg = hoursSpecial- hoursOvertime;
+		regularPayRate = hoursReg * 25;
+		overtimePayRate = hoursOvertime * (regularPayRate * 1.5f);
+		specialPayRate = hoursSpecial*50;
+		float payRate = regularPayRate + overtimePayRate + specialPayRate;
 		float result = hoursWorked * payRate;
 		return result;
 
 	}
 
-	public void setPayRate(float regularPayRate, float overtimePayRate, float hoursReg, float hoursOvertime){
-		regularPayRate = hoursReg * 25;
-		overtimePayRate = hoursOvertime * (regularPayRate * 1.5f);
-	}
-
-	public float getPayRate(float regularPayRate, float overtimePayRate) {
-		float payRate = regularPayRate + overtimePayRate;
+	public float getPayRate() {
 		return payRate;
 
 	}
 
-	public float getHoursWorked(float hoursReg, float hoursOvertime ) {
-		float hoursWorked = hoursReg + hoursOvertime;
+	public float getHoursWorked() {
 		return hoursWorked;
 	}
 
+	public void setNameRatehours(String firstName, String lastName, float payRate, float hoursWorked) {
+		this.setName(firstName, lastName);
+		this.payRate = payRate;
+		this.hoursWorked = hoursWorked;
 
-	public void setNameRatehours(String firstName, String lastName, float getPayRate, float gethoursWorked) {
-		 String name = firstName+ ' ' + lastName ;
-		 float money = getPayRate* gethoursWorked;
+	}
+
 	
-		
-	}
-	}
-
+}
